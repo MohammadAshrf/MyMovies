a6944183e548e72cee9184d29f0adad7c73d5172
Mohammad Ashraf <mohamedashrf155@gmail.com>
2026-01-28 21:15:55 +0200

    add material theme setup

---
+++ core/presentation/designsystem/src/main/java/com/example/core/presentation/designsystem/Icons.kt
@@ -0,0 +1,12 @@
+package com.example.core.presentation.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+
+val ArrowLeftIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.arrow_left)
+val LogoIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.logo)
